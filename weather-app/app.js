4a9ec4246e563ee26981ea240af9d98d97733353
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2022-10-17 19:45:25 +0330

    33

--- weather-app/app.js
+++ weather-app/app.js
@@ -6,3 +6,11 @@ request({ url: url, json: true}, (error, response) => {
     // console.log(response.body.current);
     console.log(`${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`);
 })
+
+const geocodeURL = 'http://api.positionstack.com/v1/forward?access_key=424f83dea0dc3f1072dcad218503cbcc&query=Dehdasht&limit=1'
+
+request({ url: geocodeURL, json: true}, (error, response) => {
+    const latitude = response.body.data[0].latitude
+    const longitude = response.body.data[0].longitude
+    console.log(latitude, longitude);
+})
