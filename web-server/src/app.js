77fb61d45a1061d5685f831a92ac06cfd3c2aee0
Mohsen Pasdar <mohsenpasdar@gmail.com>
2022-10-23 16:01:28 +0330

    52

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -40,9 +40,28 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'you must provide an address!'
+        })
+    }
+
     res.send({
         forecast: 'It is 15 degrees out',
-        location: 'Tehran'
+        location: 'Tehran',
+        address: req.query.search
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term!'
+        })
+    }
+    console.log(req.query.search);
+    res.send({
+        products: []
     })
 })
 
