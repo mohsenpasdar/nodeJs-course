b696e6ee54d459092e7e40f386223576019618c4
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2022-10-23 21:26:55 +0330

    57

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -40,16 +42,28 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
-    if (!req.query.search) {
+    if (!req.query.address) {
         return res.send({
             error: 'you must provide an address!'
         })
     }
 
-    res.send({
-        forecast: 'It is 15 degrees out',
-        location: 'Tehran',
-        address: req.query.search
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error }) 
+        }
+        
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error });
+            }
+
+            res.send({
+                forecast: forecastData, 
+                location,
+                address: req.query.address
+            })
+        })
     })
 })
 
