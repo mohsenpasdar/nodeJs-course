9731370ced6974cb2d8d9f26b4340c46fff2693c
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2022-10-23 22:03:20 +0330

    59

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,21 @@
 console.log('client side JS ia loaded!');
 
-fetch('http://localhost:3000/weather?address=Boston').then(response => {
-    response.json().then(data => {
-        if (data.error) {
-            console.log(data.error);
-        } else {
-            console.log(data.location);
-            console.log(data.forecast);
-        }
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
+        response.json().then(data => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                console.log(data.location);
+                console.log(data.forecast);
+            }
+        })
     })
 })
