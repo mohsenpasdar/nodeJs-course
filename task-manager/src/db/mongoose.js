092e29209eca71d1421c05d99da6389e48a6f6ee
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2022-11-10 11:13:25 +0330

    86

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,19 +1,52 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {})
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true, 
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true, 
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Emai is invalid')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password can not contain "password"!')
+            }
+        }
     } 
 })
 
 // const me = new User({
-//     name: 'Mohsen',
-//     age: 34
+//    name: 'Mohsen', 
+//    email: '   MOHSENPASDAR@gmail.com     ', 
+//    age: 34,
+//    password: 'passfdworddesss '
 // })
 
 // me.save().then(() => {
@@ -23,13 +56,19 @@ const User = mongoose.model('User', {
 // })
 
 const Task = mongoose.model('Task', {
-    description: String,
-    completed: Boolean
+    description: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
 })
 
 const task1 = new Task({
-    description: 'Walking',
-    completed: false
+    description: '   fgreading    ',
 })
 
 task1.save().then(() => {
