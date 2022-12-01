afca459a6128822397303191a6b8b0664256e729
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2022-12-01 15:14:11 +0330

    115

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -33,9 +33,9 @@ const main = async () => {
     // const task = await Task.findById('63885c46a11719c3fe232eb4')
     // await task.populate('owner')
     // console.log(task);
-    const user = await User.findById('63885a45c2f75be37f6c429f')
-    await user.populate('tasks')
-    console.log(user.tasks);
+    // const user = await User.findById('63885a45c2f75be37f6c429f')
+    // await user.populate('tasks')
+    // console.log(user.tasks);
 }
 
-main()
+// main()
