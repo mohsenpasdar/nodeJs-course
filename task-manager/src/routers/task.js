4fe14e004531203348a488d3558f1ea79dd206e1
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2022-12-01 19:29:52 +0330

    119

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -18,10 +18,18 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
+// Get /tasks?completed=true
 router.get('/tasks', auth, async (req, res) => {
     try {
-        // const tasks = await Task.find({ owner: req.user._id })
-        await req.user.populate('tasks')
+        const match = {}
+
+        if (req.query.completed) {
+            match.completed = req.query.completed === 'true'
+        }
+        await req.user.populate({
+            path: 'tasks',
+            match
+        })
         res.send(req.user.tasks)
     } catch (e) {
         res.status(500).send()
