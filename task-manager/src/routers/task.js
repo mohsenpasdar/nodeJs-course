dd300c43f7477bb64e90cac96949ff8c53bd0b2a
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2022-12-01 21:40:22 +0330

    121

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -20,19 +20,27 @@ router.post('/tasks', auth, async (req, res) => {
 
 // Get /tasks?completed=true
 // Get /tasks?limit=10&skip=10
+// GET /tasks?sortBy=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
     try {
         const match = {}
+        const sort = {}
 
         if (req.query.completed) {
             match.completed = req.query.completed === 'true'
+        }
+
+        if (req.query.sortBy) {
+            const parts = req.query.sortBy.split(':')
+            sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
         }
         await req.user.populate({
             path: 'tasks',
             match, 
             options: {
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                sort
             }
         })
         res.send(req.user.tasks)
