7f8d9a56862d38461a621e09858c15e52cd57d2e
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2022-12-01 12:10:36 +0330

    114

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -25,3 +25,17 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log(`Server is up on port ${port}!`);
 })
+
+const Task = require('./models/task')
+const User = require('./models/user')
+
+const main = async () => {
+    // const task = await Task.findById('63885c46a11719c3fe232eb4')
+    // await task.populate('owner')
+    // console.log(task);
+    const user = await User.findById('63885a45c2f75be37f6c429f')
+    await user.populate('tasks')
+    console.log(user.tasks);
+}
+
+main()
