54f51216ef4683b0e49268d739469766f5104b16
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2022-12-02 17:23:27 +0330

    125

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -77,7 +77,17 @@ router.delete('/users/me', auth, async (req, res) => {
 })
 
 const upload = multer({
-    dest: 'avatars'
+    dest: 'avatars', 
+    limits: {
+        fileSize: 1000000,
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload a jpg, jpeg, or png file'))
+        }
+
+        cb(undefined, true)
+    }
 })
 
 router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
