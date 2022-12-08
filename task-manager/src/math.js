48a7848af2a44f93946ba109e3baa846681c7142
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2022-12-09 00:09:21 +0330

    140

--- task-manager/src/math.js
+++ task-manager/src/math.js
@@ -3,6 +3,18 @@ const calcTip = (total, tipPercent = 0.2) => {
     return total + tip
 }
 
+const add = (a, b) => {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            if (a < 0 || b < 0) {
+                rej('numbers must be positive!')
+            }
+            res(a + b)
+        }, 2000);
+    })
+}
+
 module.exports = {
-    calcTip
+    calcTip, 
+    add
 }
--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,4 +1,4 @@
-const { calcTip } = require('../src/math')
+const { calcTip, add } = require('../src/math')
 
 test('should calculate total with tip', () => {
     const total = calcTip(10, 0.30)
@@ -10,4 +10,23 @@ test('should calculate total with default tip', () => {
     const total = calcTip(10)
 
     expect(total).toBe(12)
+})
+
+// test('async test demo', (done) => {
+//     setTimeout(() => {
+//         expect(1).toBe(2)
+//         done()
+//     }, 2000);
+// })
+
+test('should add two numbers', (done) => {
+    add(2, 3).then(sum => {
+        expect(sum).toBe(5)
+        done()
+    })
+})
+
+test('should add two numbers async/await', async () => {
+    const sum = await add(33, 44)
+    expect(sum).toBe(77)
 })
