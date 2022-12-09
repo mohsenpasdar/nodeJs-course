107c7b496135a7c9d6a1a989080e5c9bf1a7daca
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2022-12-09 15:18:38 +0330

    152

---
+++ chat-app/src/index.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+// app.get("/", function (req, res) {
+//   res.render("dsds");
+// });
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}.`);
+})
