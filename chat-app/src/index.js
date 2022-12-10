18dd901a776211ae487b4b1aec2bab072d7d6e6e
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2022-12-10 20:55:24 +0330

    153

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,7 +1,12 @@
 const path = require('path')
+const http = require('http')
 const express = require("express");
+const socketio = require('socket.io')
 
 const app = express();
+const server = http.createServer(app)
+const io = socketio(server)
+
 const port = process.env.PORT || 3000;
 
 // Define paths for Express config
@@ -10,10 +15,10 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
-// app.get("/", function (req, res) {
-//   res.render("dsds");
-// });
+io.on('connection', () => {
+    console.log('new web socket connection');
+})
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is up on port ${port}.`);
 })
